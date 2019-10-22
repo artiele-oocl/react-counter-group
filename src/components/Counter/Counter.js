b0615e5fcf614d03aff4fb0a067f68372bd69a02
artiele-oocl <artiele@oocl.com>
2019-10-22 13:40:14 +0800

    Additional requirement MULTIPLY_BY_2 AND DIVIDE_BY_2

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {increment, decrement} from '../actions';
+import {increment, decrement, multiplyBy2, divideBy2} from '../actions';
 
 function Counter() {  
     const counter = useSelector(state => state.counter);
     const dispatch = useDispatch();
     return(
         <div className = "counter">
-            <button onClick = {()=>dispatch(increment())}>+</button>
             <span style={{'padding-left': '20px', 'padding-right': '20px', 'color':'yellow'}}>{counter}</span>
+            <button onClick = {()=>dispatch(increment())}>+</button>
             <button onClick = {()=>dispatch(decrement())}>-</button>
+            <button onClick = {()=>dispatch(multiplyBy2())}>*2</button>
+            <button onClick = {()=>dispatch(divideBy2())}>/2</button>
         </div>
     );
 }
